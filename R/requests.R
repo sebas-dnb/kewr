e981c84c391152a3186f9079d3e81cd63ca86b92
sebasgrandesatkins <sebastian.grandes@atkinsrealis.com>
2026-07-15 16:14:10 -0500

    fix new url api

--- R/requests.R
+++ R/requests.R
@@ -153,7 +153,7 @@ get_url_ <- function(resource=c("wcvp", "powo", "knms", "ipni", "tol", "krs")) {
 
   switch(resource,
          wcvp="https://wcvp.science.kew.org/api/v1",
-         powo="http://www.plantsoftheworldonline.org/api/2",
+         powo="https://powo.science.kew.org/api/2",
          knms="http://namematch.science.kew.org/api/v2/powo/match",
          ipni="https://www.ipni.org/api/1",
          tol="https://treeoflife.kew.org/api",
